509ee68591102e6bf86e01c3692064a64f4d4364
Shantanu871 <shantanu871@gmail.com>
2026-09-25 08:10:52 -0700

    exer3

--- class8/exer3a_config_lock.py
+++ class8/exer3a_config_lock.py
@@ -12,7 +12,14 @@ Entering configuration mode
 Users currently editing the configuration:
   pyclass (pid 30316) on since 2019-03-08 18:30:51 PST
       exclusive
+3b. Use the "load" method to stage a configuration using a basic set command, for example, "set system host-name python4life".
 
+3c. Print the diff of the current configuration with the staged configuration. Your output should look similar to the following:
+[edit system]
+-  host-name srx2;
++  host-name python4life;
+
+3d. Rollback the staged configuration. Once again, print out the diff of the staged and the current configuration (which at this point should be None).
 """
 
 from jnpr.junos import Device
